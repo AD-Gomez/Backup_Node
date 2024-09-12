ac12baf449b54776f35ed95112f682ad96cb76e9
MiguelJMS <miguelmarabay19@gmail.com>
2024-09-12 17:20:09 -0400

    changes email

--- utils/sendBulkEmails.js
+++ utils/sendBulkEmails.js
@@ -1,5 +1,6 @@
 const sendMail = require('./mailer')
 
+// funcion apra el envio de emails masivos
 const sendBulkEmails = async (recipients, subject, text) => {
   for (const recipient of recipients) {
     await sendMail(recipient, subject, text)
